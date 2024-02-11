be0a289b1fbf435ce5571fcc7ceaabfdac057664
imtahirabatool <btahira183@gmail.com>
2024-02-11 18:59:52 +0500

    Your commit message here

--- API/index.js
+++ API/index.js
@@ -1,4 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connection successful!");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const app = express();
 
