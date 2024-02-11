313c6b6c0fbff99f1ac2a348fd5a82bfe8911031
imtahirabatool <btahira183@gmail.com>
2024-02-11 22:34:00 +0500

    Created middleware to handle possible errors

--- API/controllers/authController.js
+++ API/controllers/authController.js
@@ -1,7 +1,7 @@
 import User from "../models/userModel.js";
 import bcryptjs from "bcryptjs";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
@@ -11,9 +11,6 @@ export const signup = async (req, res) => {
       .status(201)
       .json({ message: "User created successfully!", user: newUser });
   } catch (error) {
-    res.status(500).json({
-      message: "An error occurred while creating the user.",
-      error: error.message,
-    });
+    next(error);
   }
 };
