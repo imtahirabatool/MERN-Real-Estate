97b3ed4f617808e292704f9c7cf5da78afcb659d
imtahirabatool <btahira183@gmail.com>
2024-02-19 01:37:14 +0500

    create user update api

--- API/controllers/userController.js
+++ API/controllers/userController.js
@@ -1,5 +1,34 @@
+import bcryptjs from "bcryptjs";
+import User from "../models/userModel.js";
 export const test = (req, res) => {
   res.json({
     message: "API route is working!",
   });
+};
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "You can only update your own account!"));
+  try {
+    const updateFields = {
+      username: req.body.username,
+      email: req.body.email,
+      avatar: req.body.avatar,
+    };
+
+    // Update password if provided
+    if (req.body.password) {
+      updateFields.password = bcryptjs.hashSync(req.body.password, 10);
+    }
+
+    const updateUser = await User.findByIdAndUpdate(
+      req.params.id,
+      { $set: updateFields },
+      { new: true }
+    );
+
+    const { password, ...rest } = updateUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
--- API/index.js
+++ API/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import userRouter from "./routes/userRoutes.js";
 import authRouter from "./routes/authRoutes.js";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 mongoose
@@ -16,6 +17,7 @@ mongoose
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
