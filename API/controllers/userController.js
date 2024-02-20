cc59b4a6af4b6928709ea81adaa5b2193682e4c5
imtahirabatool <btahira183@gmail.com>
2024-02-21 01:59:18 +0500

    Added delete user functionality!

--- API/controllers/userController.js
+++ API/controllers/userController.js
@@ -1,5 +1,6 @@
 import bcryptjs from "bcryptjs";
 import User from "../models/userModel.js";
+import { errorHandler } from "../utils/error.js";
 export const test = (req, res) => {
   res.json({
     message: "API route is working!",
@@ -28,6 +29,18 @@ export const updateUser = async (req, res, next) => {
 
     const { password, ...rest } = updateUser._doc;
     res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "You can only delete your account!"));
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.clearCookie("access_token");
+    res.status(200).json("User has been deleted!");
   } catch (error) {
     next(error);
   }
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -11,6 +11,9 @@ import {
   updateUserStart,
   updateUserSuccess,
   updateUserFailure,
+  deleteUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
 } from "../user/userSlice";
 
 export default function Profile() {
@@ -84,6 +87,23 @@ export default function Profile() {
     }
   };
 
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currUser._id}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -143,7 +163,13 @@ export default function Profile() {
         </button>
       </form>
       <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer"> Delete Account</span>
+        <span
+          onClick={handleDeleteUser}
+          className="text-red-700 cursor-pointer"
+        >
+          {" "}
+          Delete Account
+        </span>
         <span className="text-red-700 cursor-pointer"> Sign Out</span>
       </div>
 
