cf5a30d6600e2634a922f9ae84b5495f7c9dcf93
imtahirabatool <btahira183@gmail.com>
2024-03-22 17:41:59 +0500

    Create search API route!

--- API/routes/userRoutes.js
+++ API/routes/userRoutes.js
@@ -4,7 +4,7 @@ import {
   test,
   updateUser,
   getUserListings,
-  getUser,
+  getUser,getListings,
 } from "../controllers/userController.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -15,4 +15,5 @@ router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
 router.get("/listings/:id", verifyToken, getUserListings);
 router.get("/:id", verifyToken, getUser);
+router.get("/get", getListings)
 export default router;
