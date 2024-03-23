3211b81ce37c9b62de02983e267ab46df3fcfc51
imtahirabatool <btahira183@gmail.com>
2024-03-23 13:16:17 +0500

    Complete search page UI!

--- API/routes/userRoutes.js
+++ API/routes/userRoutes.js
@@ -4,7 +4,8 @@ import {
   test,
   updateUser,
   getUserListings,
-  getUser,getListings,
+  getUser,
+  getListings,
 } from "../controllers/userController.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -15,5 +16,5 @@ router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
 router.get("/listings/:id", verifyToken, getUserListings);
 router.get("/:id", verifyToken, getUser);
-router.get("/get", getListings)
+router.get("/get", getListings);
 export default router;
