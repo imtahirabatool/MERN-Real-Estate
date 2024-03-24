416144f1d1427cf9e989dd0fd420b660b093c495
imtahirabatool <btahira183@gmail.com>
2024-03-24 21:23:03 +0500

    add pre installation

--- API/index.js
+++ API/index.js
@@ -5,6 +5,7 @@ import authRouter from "./routes/authRoutes.js";
 import listingRoute from "./routes/listingRoute.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
 
 mongoose
@@ -15,6 +16,8 @@ mongoose
   .catch((error) => {
     console.log(error);
   });
+
+const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
@@ -27,6 +30,11 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRoute);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((error, req, res, next) => {
   const statusCode = error.statusCode || 500;
